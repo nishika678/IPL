78310c89b422c5c02d223cfbd80f739879387137
nishika678 <147605762+nishika678@users.noreply.github.com>
2025-09-25 01:29:55 +0530

    Update IplApplication.java

--- backend/src/main/java/com/wecp/progressive/IplApplication.java
+++ backend/src/main/java/com/wecp/progressive/IplApplication.java
@@ -1,3 +1,17 @@
+
+package com.wecp.progressive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplApplication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Ipl Progressive Project!");
+        //System.out.println("Hello IPL Progressive Project");
+       SpringApplication.run(IplApplication.class, args);
+    }
+}
 package com.wecp.progressive;
 
 public class IplApplication {
