faad7d81f44b11a6f9e383300691a5463a8ef6d0
nishika678 <147605762+nishika678@users.noreply.github.com>
2025-09-25 01:26:12 +0530

    Update MatchService.java

--- backend/src/main/java/com/wecp/progressive/service/MatchService.java
+++ backend/src/main/java/com/wecp/progressive/service/MatchService.java
@@ -1,3 +1,28 @@
+
+package com.wecp.progressive.service;
+
+import java.sql.SQLException;
+import com.wecp.progressive.entity.Match;
+
+import java.util.List;
+
+public interface MatchService {
+
+    List<Match> getAllMatches() throws SQLException;
+
+    Match getMatchById(int matchId) throws SQLException;
+
+    Integer addMatch(Match match) throws SQLException;
+
+    void updateMatch(Match match) throws SQLException;
+
+    void deleteMatch(int matchId) throws SQLException;
+
+    //Do not implement these methods in MatchServiceImplJdbc.java class
+    default List<Match> getAllMatchesByStatus(String status) throws SQLException{
+        return null;
+    }
+}
 package com.wecp.progressive.service;
 
 import com.wecp.progressive.entity.Match;
