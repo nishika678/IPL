9b0f435382b08f4a6f56be8245c2f89e0bd4e9c6
nishika678 <147605762+nishika678@users.noreply.github.com>
2025-09-25 01:29:08 +0530

    Update TeamController.java

--- backend/src/main/java/com/wecp/progressive/controller/TeamController.java
+++ backend/src/main/java/com/wecp/progressive/controller/TeamController.java
@@ -1,3 +1,79 @@
+
+package com.wecp.progressive.controller;
+
+import com.wecp.progressive.entity.Team;
+import com.wecp.progressive.service.impl.TeamServiceImplArraylist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/team")
+public class TeamController {
+
+    @Autowired
+    @Qualifier("teamServiceImplArrayList")
+    private TeamServiceImplArraylist teamServiceImplArrayList;
+
+    @GetMapping()
+    public ResponseEntity<List<Team>> getAllTeams() {
+        List<Team> teams = teamServiceImplArrayList.getAllTeams();
+        return new ResponseEntity<>(teams, HttpStatus.OK);
+    }
+
+    // @GetMapping("/{teamId}")
+    // public ResponseEntity<Team> getTeamById(@PathVariable int teamId) {
+    //     Team team = teamServiceImplArrayList.getTeamById(teamId);
+    //     return new ResponseEntity<>(team, HttpStatus.OK);
+    // }
+
+    @PostMapping
+    public ResponseEntity<Integer> addTeam(@RequestBody Team team) 
+    {
+        int t = teamServiceImplArrayList.addTeam(team);
+        return new ResponseEntity<>(t, HttpStatus.OK);
+    }
+
+    //@PutMapping("/{teamId}")
+    public ResponseEntity<Void> updateTeam(int teamId, Team team) {
+        return null;
+    }
+
+    //@DeleteMapping("/{teamId}")
+    public ResponseEntity<Void> deleteTeam(int teamId) {
+        return null;
+    }
+
+    @GetMapping("/fromArrayList")
+    public ResponseEntity<List<Team>> getAllTeamsFromArrayList() {
+        List<Team> teamList = teamServiceImplArrayList.getAllTeams();
+        return new ResponseEntity<>(teamList, HttpStatus.OK);
+    }
+
+    @PostMapping("/toArrayList")
+    public ResponseEntity<Void> addTeamToArrayList(@RequestBody Team team) {
+        int t = teamServiceImplArrayList.addTeam(team);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fromArrayList/sorted")
+    public ResponseEntity<List<Team>> getAllTeamsSortedByNameFromArrayList() {
+        List<Team> team = teamServiceImplArrayList.getAllTeamsSortedByName();
+        return new ResponseEntity<>(team, HttpStatus.OK);
+    }
+}
 package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Team;
